29ffebaff2fb4e5ca97b44371b39b496d51e0be4
sanjeevbora11 <45086620+sanjeevbora11@users.noreply.github.com>
2021-09-16 10:41:31 +0530

    Add files via upload

---
+++ progpalindrome.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 18:54:19 2020
+
+@author: Hp
+"""
+s1=input("Enter string: ")
+s2=s1[::-1]
+if s1==s2:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
